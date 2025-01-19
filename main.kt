1a1eb58d241173602de4daf41c730518d1f7ef7a
Enzo <enzogambrosio@hotmail.com>
2025-01-19 14:04:20 -0300

    Atualizar o main.kt
    Feito a verificação se o board[] está cheio( isFull() ). Próximo objetivo verificar se há alguma vitória

--- main.kt
+++ main.kt
@@ -1,12 +1,13 @@
+
 // i:                0    1    2    3    4    5   6    7    8
-val board = arrayOf("0", "0", "0", "0", "0", "0","0", "0", "0")
+val board = arrayOf("0", "0", "0", "0", "0", "0", "0", "0", "0")
 val playerX = "X"
 val playerO = "O"
-var victory: Boolean = false 
+var victory: Boolean = false
 
 fun divider() = println("                      ")
 
-fun showTab(){
+fun showTab() {
     println(board[0] + " | " + board[1] + " | " + board[2])
     println("--+---+--")
     println(board[3] + " | " + board[4] + " | " + board[5])
@@ -15,40 +16,63 @@ fun showTab(){
     divider()
 }
 
-fun moveValid(move: Int):Boolean{
-    if (move-1 <0 || move-1 >8){
+fun moveValid(move: Int): Boolean {
+    if (move - 1 < 0 || move - 1 > 8) {
         println("Invalid move. Try again:")
         return false
     }
-    if (board[move-1] !== "0"){
+    if (board[move - 1] !== "0") {
         println("Invalid move. Try again:")
         return false
     }
     return true
 }
 
-fun playerMove(player:String, move:Int) {
-    board[move-1] = player
+fun playerMove(player: String, move: Int) {
+    board[move - 1] = player
 }
 
-fun main(){
-    var move:Int
+fun isFull(): Boolean {
+    var i: Int = 0
+    for (element in board) {
+        if (element == "0") {
+            i = i + 1
+        }
+    }
+    if (i > 0) {
+        return false
+    }
+    return true
+}
+
+fun main() {
+    var move: Int
     println("Bem vindo ao jogo da velha")
     showTab()
-    while (victory == false){
-    //Player X
+    while (victory == false) {
+         if (isFull() == true) {
+            println("Tabela cheia")
+            println("Nenhum jogador venceu")
+        }
+        // Player X
         println("Jogada do jogador X:")
         do {
             move = readLine()!!.toInt()
-        }while (moveValid(move) == false)
+        } while (moveValid(move) == false)
         playerMove(playerX, move)
         showTab()
-    //Player 0
+  
+        // Player 0 
+        if (isFull() == true) {
+            println("Tabela cheia")
+            println("Nenhum jogador venceu")
+        }
         println("Jogada do jogador O:")
         do {
             move = readLine()!!.toInt()
-        }while (moveValid(move) == false)
+        } while (moveValid(move) == false)
         playerMove(playerO, move)
         showTab()
     }
+    println("Fim de jogo")
 }
